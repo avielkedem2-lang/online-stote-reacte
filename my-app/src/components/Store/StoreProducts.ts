4a0221191ea699ba91b7dd2e8fd626af89d00731
אביאל קדם <avielkedem2@gmail.com>
2026-09-06 10:49:36 +0300

    add zustand StoreProducts

---
+++ my-app/src/components/Store/StoreProducts.ts
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+
+type Product = {
+    id: number,
+    title: string,
+    price: number,
+    description: string,
+    category: string,
+    image: string
+}
+
+type ProductsTypes = {
+    products: Product[],
+    setProducts: (products: Product[]) => void,
+}
+
+export const useProductsStore = create<ProductsTypes>((set) => ({
+    products: [],
+    setProducts: (products: Product[]) => set(() => ({products})) 
+}))
